ff7a0b74faf91ca3a3d0f1bca4fd4b39ae707c20
Ryan Carr <ryan.carr@ryca.dev>
2024-12-20 11:42:11 -0800

    Initial Revision - section 2 completed

---
+++ Assets/Scripts/GamePiece.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class GamePiece : MonoBehaviour
+{
+
+    public int xIndex;
+    public int yIndex;
+
+    Board m_board;
+
+    bool m_isMoving = false;
+
+    public InterpType interpolation = InterpType.SmootherStep;
+
+    public enum InterpType {
+        Linear,
+        EaseOut, 
+        EaseIn, 
+        SmoothStep,
+        SmootherStep
+    }
+
+
+    public MatchValue matchValue;
+    public enum MatchValue {
+        Yellow,
+        Blue,
+        Cyan,
+        Green,
+        Magenta,
+        Purple,
+        Red,
+        Teal,
+        Wild
+    }
+
+    public void Init(Board board) {
+        m_board = board;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (Input.GetKeyDown(KeyCode.RightArrow)) {
+        //     Move((int)transform.position.x + 1, (int)transform.position.y, 0.5f);
+        // }
+
+        // if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+        //     Move((int)transform.position.x -1, (int)transform.position.y, 0.5f);
+        // }
+
+
+
+    }
+
+    public void SetCoord(int x, int y) {
+        xIndex = x;
+        yIndex = y;
+    }
+
+    public void Move(int destX, int destY, float timeToMove) {
+        
+        if (!m_isMoving) {
+            StartCoroutine(MoveRoutine(new Vector3(destX, destY, 0), timeToMove));
+        }
+    }
+
+    IEnumerator MoveRoutine(Vector3 destination, float timeToMove) {
+        
+        Vector3 startPosition = transform.position;
+        bool reachedDestination = false;
+
+        float elapsedTime = 0f;
+
+        m_isMoving = true;
+
+        while (!reachedDestination) {
+
+            // move the game piece toward it's destination
+            
+            // if we are close enough to the destination, complete
+            if (Vector3.Distance(transform.position, destination) < 0.01f) {
+                reachedDestination = true;
+                
+                if (m_board != null) {
+                    m_board.PlaceGamePiece(this, (int) destination.x, (int) destination.y);
+                }
+
+                break;
+            }
+
+            elapsedTime += Time.deltaTime;
+            float t = Mathf.Clamp(elapsedTime / timeToMove, 0f, 1f);  // NOTE: Vector3 already has a clamp internally
+
+            switch(interpolation) {
+                case InterpType.Linear:
+                    break;
+                case InterpType.EaseOut:
+                    t = Mathf.Sin(t * (float)Math.PI * 0.5f);
+                    break;
+                case InterpType.EaseIn:
+                    t = 1 - Mathf.Cos(t * (float)Math.PI * 0.5f);
+                    break;
+                case InterpType.SmoothStep:
+                    t = t * t * (3 - 2 * t);
+                    break;
+                case InterpType.SmootherStep:
+                    t = t * t * t * (t * (t * 6 - 15) + 10);
+                    break; 
+            }
+
+            // t = Mathf.Sin(t ( Math.PI * 0.5));
+            // t = 1 - Mathf.Cos(t * Math.PI * 0.5f);
+
+            transform.position = Vector3.Lerp(startPosition, destination, t);
+
+
+            // wait until next frame
+            yield return null;
+        }
+
+        m_isMoving = false;
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public int xIndex;
+    public int yIndex;
+
+    Board m_board;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public void Init(int x, int y, Board board) {
+        xIndex = x;
+        yIndex = y;
+        m_board = board;
+    }
+
+    void OnMouseDown() {
+        if (m_board != null) {
+            m_board.ClickTile(this);
+        }
+    }
+
+    void OnMouseEnter() {
+        if (m_board != null) {
+            m_board.DragToTile(this);
+        }
+    }
+
+    void OnMouseUp() {
+        if (m_board != null) {
+            m_board.ReleaseTile();
+        }
+    }
+}
